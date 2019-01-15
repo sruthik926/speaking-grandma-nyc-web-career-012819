6f48a157d344e95da71c312b8711b649d30e2abe
Sruthi Krishna <sruthik926@gmail.com>
2019-01-15 03:28:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(str)
-  if str == "I LOVE YOU GRANDMA"
+  if str == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif str == str.upcase
     return "NO, NOT SINCE 1938!"
